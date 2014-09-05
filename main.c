57449336a90344d3c4d35d01858ee4d7c92a7533
wujiang1 <wujiang1@staff.sina.com.cn>
2014-09-05 10:24:47 +0800

    完成Erasure Coding的范德蒙矩阵基本编解码代码实现及测试

--- main.c
+++ main.c
@@ -1,87 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "matrix_opt.h"
 
+
+
 int main()
 {
-    FILE *fp, *fp2;
-    int i,j,n=0,m;
-    char *fname;
-    char  **data,**decoding,*curdir;
-    char  **coding,**coding2;
-    int blocksize = 320;
-    int van_encode_matrix[3][10]=
-    {
-        { 1,1,1,1,1,1,1,1,1,1},
-        { 1,2,3,4,5,6,7,8,9,10},
-        { 1,4,9,16,25,36,49,64,81,100}
-    };
-    int van_decode_matrix[10][10] =
-    {
-        { 1 , 2 , 3 , 4 , 5 , 6 , 7, 8 , 2 ,-1 },
-        {-2 ,-3,-4 ,-5 ,-6,-7 ,-8 ,-9 ,-1 , 1 },
-        { 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 },
-        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 },
-    };
 
-    /* Allocate data and coding */
-    data = ( char  **)malloc(sizeof( char *)*K);
-    decoding = ( char  **)malloc(sizeof( char *)*K);
-    coding = ( char  **)malloc(sizeof( char *)*M);
-    for (i = 0; i < M; i++)
-    {
-        coding[i] = ( char  *)calloc(blocksize,sizeof( char ));
-    }
-    for(i=0; i<K; i++)
-    {
-        data[i] = ( char  *)calloc(blocksize,sizeof( char ));
-        decoding[i] = ( char  *)calloc(blocksize,sizeof( char ));
-        if(i==0)
-        {
-            data[i] = "abcdefghij";
-        }
-        if(i==1){
-            data[i] = "klmnopqrst";
-        }
-       // printf("%d = %s\nlen = %d\n\n",i,data[i],strlen(data[i]));
-    }
-    curdir = (char*)malloc(sizeof(char)*1000);
-    fname = (char*)malloc(sizeof(char)*200);
-    getcwd(curdir, 1000);
-
-    //求逆矩阵
-    //rinv_test();
-
-    //encode
-    van_matrix_encode(10,3,van_encode_matrix,data,coding,blocksize);
-    for(i=0;i<3;i++){
-        printf("\ncoding[%d] =%d=%s",i,strlen(coding[i]),coding[i]);
-        sprintf(fname, "%s/m%d", curdir, i);
-        save_coding(fname,coding[i],blocksize);
-    }
-    printf("\n\n");
-
-    //decode
-    data[0]=( char  *)calloc(blocksize,sizeof( char ));
-    data[1]=( char  *)calloc(blocksize,sizeof( char ));
-    data[8] = coding[0];
-    data[9] = coding[1];
-//    for(i=0;i<10;i++){
-//        printf("%d=%s\n",i,data[i]);
-//    }
-    van_matrix_encode(10,10,van_decode_matrix,data,decoding,blocksize);
-    for (i=0; i<K; i++)
-    {
-        printf("decoding[%d] = %s\n",i,decoding[i]);
-    }
-    printf("\n");
-
+    ec_test();
     return 0;
 }
 
--- matrix_opt.c
+++ matrix_opt.c
@@ -2,6 +2,91 @@
 #include <stdlib.h>
 
 #include "matrix_opt.h"
+
+void ec_test()
+{
+    FILE *fp, *fp2;
+    int i,j,n=0,m;
+    char *fname;
+    char  **data,**decoding,*curdir;
+    char  **coding,**coding2;
+    int blocksize = 320;
+    int van_encode_matrix[3][10]=
+    {
+        { 1,1,1,1,1,1,1,1,1,1},
+        { 1,2,3,4,5,6,7,8,9,10},
+        { 1,4,9,16,25,36,49,64,81,100}
+    };
+    int van_decode_matrix[10][10] =
+    {
+        { 1 , 2 , 3 , 4 , 5 , 6 , 7, 8 , 2 ,-1 },
+        {-2 ,-3,-4 ,-5 ,-6,-7 ,-8 ,-9 ,-1 , 1 },
+        { 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 },
+        { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 },
+    };
+
+    /* Allocate data and coding */
+    data = ( char  **)malloc(sizeof( char *)*K);
+    decoding = ( char  **)malloc(sizeof( char *)*K);
+    coding = ( char  **)malloc(sizeof( char *)*M);
+    for (i = 0; i < M; i++)
+    {
+        coding[i] = ( char  *)calloc(blocksize,sizeof( char ));
+    }
+    for(i=0; i<K; i++)
+    {
+        data[i] = ( char  *)calloc(blocksize,sizeof( char ));
+        decoding[i] = ( char  *)calloc(blocksize,sizeof( char ));
+        if(i==0)
+        {
+            data[i] = "abcdefghij";
+        }
+        if(i==1)
+        {
+            data[i] = "klmnopqrst";
+        }
+        // printf("%d = %s\nlen = %d\n\n",i,data[i],strlen(data[i]));
+    }
+    curdir = (char*)malloc(sizeof(char)*1000);
+    fname = (char*)malloc(sizeof(char)*200);
+    getcwd(curdir, 1000);
+
+    //求逆矩阵
+    rinv_test();
+    //exit(EXIT_SUCCESS);
+
+    //encode
+    van_matrix_encode(10,3,van_encode_matrix,data,coding,blocksize);
+    for(i=0; i<3; i++)
+    {
+        printf("\ncoding[%d] =%d=%s",i,strlen(coding[i]),coding[i]);
+        sprintf(fname, "%s/m%d", curdir, i);
+        save_coding(fname,coding[i],blocksize);
+    }
+    printf("\n\n");
+
+    //decode
+    data[0]=( char  *)calloc(blocksize,sizeof( char ));
+    data[1]=( char  *)calloc(blocksize,sizeof( char ));
+    data[8] = coding[0];
+    data[9] = coding[1];
+//    for(i=0;i<10;i++){
+//        printf("%d=%s\n",i,data[i]);
+//    }
+    van_matrix_encode(10,10,van_decode_matrix,data,decoding,blocksize);
+    for (i=0; i<K; i++)
+    {
+        printf("decoding[%d] = %s\n",i,decoding[i]);
+    }
+    printf("\n");
+}
+
 void galois_region_xor( char *r1, int l2,char *r3,int nbytes)
 {
     long *l1;
@@ -223,7 +308,7 @@ void rinv_test()
 //    };
     double a[10][10] =
     {
-        {0,0,1,0,0,0,0,0,0,0},
+        //{0,0,1,0,0,0,0,0,0,0},
         {0,0,0,1,0,0,0,0,0,0},
         {0,0,0,0,1,0,0,0,0,0},
         {0,0,0,0,0,1,0,0,0,0},
@@ -233,7 +318,7 @@ void rinv_test()
         {0,0,0,0,0,0,0,0,0,1},
         {1,1,1,1,1,1,1,1,1,1},
         {1,2,3,4,5,6,7,8,9,10},
-//        {1,4,9,16,25,36,49,64,81,100}
+        {1,4,9,16,25,36,49,64,81,100}
     };
 //    double a[3][3] =
 //    {
@@ -273,21 +358,21 @@ void rinv_test()
             printf("{");
             for (j=0; j<K; j++)
             {
-                printf("%4.1f ",a[i][j]);
+                printf("%4.1f\t ",a[i][j]);
                 if(j<9) printf(",");
             }
             printf("},\n");
         }
         printf("\n");
 
-        printf("MAT AA- IS:\n");
-        trmul(b,a,K,K,K,c);
-        for (i=0; i<K; i++)
-        {
-            for (j=0; j<K; j++)
-                printf("%3.0f ",c[i][j]);
-            printf("\n");
-        }
+//        printf("MAT AA- IS:\n");
+//        trmul(b,a,K,K,K,c);
+//        for (i=0; i<K; i++)
+//        {
+//            for (j=0; j<K; j++)
+//                printf("%3.0f ",c[i][j]);
+//            printf("\n");
+//        }
     }
 }
 
