1e248759ea7d2631fcaa13675f4f90d8238ea76a
wujiang1 <wujiang1@staff.sina.com.cn>
2014-11-27 15:11:13 +0800

    add new files

--- common/unit_test.h
+++ common/unit_test.h
@@ -1,9 +1,31 @@
 #ifndef UNIT_TEST_H_INCLUDED
 #define UNIT_TEST_H_INCLUDED
 
+#include "queue.h"
+
 #define likely(x)	__builtin_expect(!!(x), 1)
 #define unlikely(x)	__builtin_expect(!!(x), 0)
 #define nf_align(type, size, align) (((size)+(align)-1) & ((type)~((align)-1)))
+typedef STAILQ_HEAD(block_frame_head, block_frame) bf_head;
+typedef struct buffer_pool {
+	int							used;
+	int							total;
+	bf_head						free_buff;
+	bf_head						free_frame;
+
+	int							bf_len;
+	int							bf_off;
+	void 						**bf_page;
+} nf_bp_t;
+
+/*********************** Chained buffer ************************/
+typedef struct buffer {
+	int							len;
+	int							off;
+	char						*last;
+	nf_bp_t						*pool;
+	STAILQ_HEAD(, block_frame)	head;
+} nf_buff_t;
 
 void get_param(int argc,char *argv[]);
 void print_file_Statfs();
