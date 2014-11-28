a5886ceb593fbb279957aa4aec27db13af27fc4d
wujiang1 <wujiang1@staff.sina.com.cn>
2014-11-28 12:03:22 +0800

    test

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 #include "algorithm/sort.h"
 #include "libevent/libevent_server.h"
 #include "string/str_test.h"
+#include "common/unit_test.h"
 
 int main(int argc,char *argv[])
 {
 
-    sort_test();
+    unlikely_test();
 
     return EXIT_SUCCESS;
 }
