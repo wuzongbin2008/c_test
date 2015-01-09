34a215c2d2224d5154cff4cdcb87d5804906e134
wujiang1 <wujiang1@staff.sina.com.cn>
2015-01-09 14:54:09 +0800

    file stat

--- file/file_test.c
+++ file/file_test.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
-#include<sys/file.h>
+#include <sys/file.h>
+#include <sys/stat.h>
 #include <errno.h>
+#include <limits.h>
 
 #include "file_test.h"
+
+void file_size()
+{
+    struct stat f_st;
+    off_t size;
+    int fd, rt;
+    char *filename = "/project/c/c_test/file/data/nginx.conf";
+
+    fd = open(filename, O_RDONLY, 0);
+    rt = fstat(fd, &f_st);
+    rt = stat(filename, &f_st);
+    size = f_st.st_size;
+
+    printf("rt: %d\nsize: %d\n", rt, size);
+
+}
 
 char * get_path()
 {
