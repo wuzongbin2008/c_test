07280169bd9a1d400b99e04f09d6b70693918c61
wujiang1 <wujiang1@staff.sina.com.cn>
2015-01-12 11:21:20 +0800

    add statement_t

---
+++ common/statement_t.c
@@ -0,0 +1,18 @@
+#include "statement_t.h"
+
+void switch_test()
+{
+    char ch;
+    ch = ';';
+    switch(ch){
+    case ';':
+    case 'a':
+        printf("a\n");
+        break;
+    case 'b':
+        printf("b\n");
+        break;
+    default:
+        printf("default: %c\n",ch);
+    }
+}
---
+++ common/statement_t.h
@@ -0,0 +1,6 @@
+#ifndef STATEMENT_T_H_INCLUDED
+#define STATEMENT_T_H_INCLUDED
+
+void switch_test();
+
+#endif // STATEMENT_T_H_INCLUDED
