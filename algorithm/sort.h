2e65704a03dc98032ba0098035edef35e0f75aa7
wujiang1 <wujiang1@staff.sina.com.cn>
2015-01-13 09:53:33 +0800

    insert_srot test

--- algorithm/sort.h
+++ algorithm/sort.h
@@ -1,6 +1,65 @@
-#include <stdio.h>
-#include"c1.h"
-#include"c10-3.h"
+// c1.h (程序名)
+#include<string.h>
+#include<ctype.h>
+#include<malloc.h> // malloc()等
+#include<limits.h> // INT_MAX等
+#include<stdio.h> // EOF(=^Z或F6),NULL
+#include<stdlib.h> // atoi()
+//#include<io.h> // eof()
+#include<math.h> // floor(),ceil(),abs()
+// #include<process.h> // exit()
+//#include<iostream.h> // cout,cin
+
+// 函数结果状态代码
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+// c10-3.h 基数排序的数据类型
+#define MAX_NUM_OF_KEY 8 // 关键字项数的最大值
+#define RADIX 10 // 关键字基数，此时是十进制整数的基数
+#define MAX_SPACE 1000
+
+// #define OVERFLOW -2 因为在math.h中已定义OVERFLOW的值为3,故去掉此行
+typedef int Status; // Status是函数的类型,其值是函数结果状态代码，如OK等
+typedef int Boolean; // Boolean是布尔类型,其值是TRUE或FALSE
+// alg10-11.cpp 链式基数排序
+typedef int InfoType; // 定义其它数据项的类型
+typedef int KeyType; // 定义RedType类型的关键字为整型
+typedef char KeysType; // 定义关键字类型为字符型
+// c10-1.h 待排记录的数据类型
+#define MAXSIZE 20 // 一个用作示例的小顺序表的最大长度
+typedef int KeyType; // 定义关键字类型为整型
+
+typedef struct  // 记录类型
+{
+    KeyType key; // 关键字项
+    InfoType otherinfo; // 其它数据项，具体类型在主程中定义
+} RedType;
+
+typedef struct  // 顺序表类型
+{
+    RedType r[MAXSIZE+1]; // r[0]闲置或用作哨兵单元
+    int length; // 顺序表长度
+} SqList;
+
+typedef struct  // 静态链表的结点类型
+{
+    KeysType keys[MAX_NUM_OF_KEY]; // 关键字
+    InfoType otheritems; // 其它数据项
+    int next;
+} SLCell;
+
+typedef struct  // 静态链表类型
+{
+    SLCell r[MAX_SPACE]; // 静态链表的可利用空间，r[0]为头结点
+    int keynum; // 记录的当前关键字个数
+    int recnum; //  静态链表的当前长度
+} SLList;
+typedef int ArrType[RADIX]; // 指针数组类型
+
+
 
 void radix_sort();
 void InitList(SLList *L,RedType D[],int n);
@@ -23,6 +82,7 @@ int less(int a,int b);
 void simple_select_sort();
 
 void straight_insert_sort();
+void binary_insert_sort(SqList *L);
 void shell_sort();
 
 void quick_sort(int k[],int n);
